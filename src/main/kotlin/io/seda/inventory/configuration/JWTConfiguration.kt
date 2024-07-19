d97b2d9713b81ddbb9f24d1edd7435221c97d670
syeyoung <cyoung06@naver.com>
2024-07-19 14:27:16 +0900

    - use ssm

--- src/main/kotlin/io/seda/inventory/configuration/JWTConfiguration.kt
+++ src/main/kotlin/io/seda/inventory/configuration/JWTConfiguration.kt
@@ -4,6 +4,8 @@ import com.auth0.jwt.algorithms.Algorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.services.ssm.SsmClient
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest
 import java.security.KeyFactory
 import java.security.KeyPair
 import java.security.KeyPairGenerator
@@ -17,19 +19,24 @@ import java.util.*
 @Configuration
 class JWTConfiguration {
     @Value("\${jwt.private}")
-    lateinit var privateKey: String;
+    lateinit var privateKeyName: String;
 
     @Value("\${jwt.public}")
-    lateinit var publicKey: String;
+    lateinit var publicKeyName: String;
 
     @Value("\${jwt.random}")
     lateinit var randomKey: String;
     @Bean
     fun keyPair(): KeyPair{
+        var client: SsmClient = SsmClient.create();
+
         if (randomKey.toBoolean()) {
             val keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             return keyPairGenerator.generateKeyPair();
         } else {
+            val publicKey = client.getParameter(GetParameterRequest.builder().withDecryption(true).name(publicKeyName).build()).parameter().value();
+            val privateKey = client.getParameter(GetParameterRequest.builder().withDecryption(true).name(privateKeyName).build()).parameter().value();
+
             val privateKeySpec = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey));
             val publicKeySpec = X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
             val keyFactory = KeyFactory.getInstance("RSA");
