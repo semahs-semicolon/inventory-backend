be136c22be33cf627f03b6a26f4d9905532653ea
thezoot3 <thezoot3@gmail.com>
2024-08-11 11:39:27 +0900

    debugg

--- src/main/kotlin/io/seda/inventory/auth/JWTAuthenticationFilter.kt
+++ src/main/kotlin/io/seda/inventory/auth/JWTAuthenticationFilter.kt
@@ -26,7 +26,7 @@ class JWTReactiveAuthorizationFilter(private val jwtService: JWTService) : WebFi
 
         try {
             val token: JWTUserDetails = jwtService.validateJWT(authHeader.substring(7))
-            val auth = UsernamePasswordAuthenticationToken(token, authHeader.substring(7), listOf())
+            val auth = UsernamePasswordAuthenticationToken(token, authHeader.substring(7), token.getAuthorities())
             println(token.getAuthorities())
             println(auth.authorities)
             return chain.filter(exchange).contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth))
