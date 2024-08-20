d29983b57a7df5af4c16b40550e6c94772b1d096
syeyoung <42869671+cyoung06@users.noreply.github.com>
2024-08-21 08:33:51 +0900

    Update Identifier.kt
    Fix nullability in Identifier.kt

--- src/main/kotlin/io/seda/inventory/data/Identifier.kt
+++ src/main/kotlin/io/seda/inventory/data/Identifier.kt
@@ -3,15 +3,13 @@ package io.seda.inventory.data
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.lang.Nullable
 
 @Table(name = "identifier")
 data class Identifier(
     @Id
     var id: Long? = null,
     var identifierCode: String,
-    @Nullable
-    var metadata: String = ""
+    var metadata: String? = ""
 )
 
 interface IdentifierRepository: CoroutineCrudRepository<Identifier, String> {
